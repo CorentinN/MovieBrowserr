682109f8d9bf83c4b611c011ea92c698b4f3b854
CorentinN <noirot.co@gmail.com>
2018-04-23 19:19:48 +0200

    test to get back my stuff

---
+++ src/components/Main.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../styles/Normalize.css';
+
+let pagination = undefined;
+class Main extends React.Component {
+    // handleReloader(){
+    //     this.props.handleReload();
+    // }
+    handleSubmitId(id,e){
+        this.props.handleSubmitId(id);
+    }
+    loadMore(e,pagination){
+        this.props.loadMore();
+        pagination++
+    }
+    render() { 
+        return ( 
+            <div className="main">
+                {this.props.title.map(title =>
+                    <Link to={"/film/"+ title.id}
+                        className="film-wrapper"
+                        key={title.title}
+                        onClick={this.handleSubmitId.bind(this, title.id)}
+                        >
+                        <img alt={title.title} src={"https://image.tmdb.org/t/p/w500" + title.poster_path} />
+                    </Link>
+                )}
+                <div 
+                className="btn-load-more"
+                onClick={this.loadMore.bind(this,pagination )}
+                >
+                    Load More
+                </div>
+            </div>
+         )
+    }
+}
+
+export default Main;
